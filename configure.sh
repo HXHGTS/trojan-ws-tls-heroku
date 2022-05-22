20aa5acaa1ed8cc41d20c4536018c04990c44b7d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-05-22 16:01:29 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -31,7 +31,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws"
             },
                 "wsSettings": {
-                    "path": "/$WSPATH"
+                    "path": "$WSPATH"
                 }
         }
     ],
